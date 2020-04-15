b3d8439bbbb4c4fea110c97a708f7bc34867402c
Your Name <nizarmu7@gmail.com>
2020-04-15 15:54:00 +0700

    Pembuatan Activity Register

---
+++ app/src/main/java/com/akb/latihan2_10117149/HomeActivity.kt
@@ -0,0 +1,4 @@
+package com.akb.latihan2_10117149
+
+class HomeActivity {
+}
--- app/src/main/java/com/akb/latihan2_10117149/RegisterActivity.kt
+++ app/src/main/java/com/akb/latihan2_10117149/RegisterActivity.kt
@@ -1,4 +1,12 @@
 package com.akb.latihan2_10117149
 
-class RegisterActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
 }
