0202a6fa9899400bc66b9257f5276f17fab0e54b
Your Name <nizarmu7@gmail.com>
2020-04-15 16:58:16 +0700

    Pembuatan Activity Verify

--- app/src/main/java/com/akb/latihan2_10117149/AlmostActivity.kt
+++ app/src/main/java/com/akb/latihan2_10117149/AlmostActivity.kt
@@ -2,10 +2,17 @@ package com.akb.latihan2_10117149
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_almost.*
 
 class AlmostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_almost)
+
+        btn_Verify.setOnClickListener {
+            val intent = Intent(this, VerifyActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/akb/latihan2_10117149/VerifyActivity.kt
+++ app/src/main/java/com/akb/latihan2_10117149/VerifyActivity.kt
@@ -1,4 +1,11 @@
 package com.akb.latihan2_10117149
 
-class VerifyActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VerifyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verify)
+    }
 }
