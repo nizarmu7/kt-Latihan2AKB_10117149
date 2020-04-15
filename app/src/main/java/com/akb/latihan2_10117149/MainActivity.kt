6f9af4d93a54bb907a065eb36f22ca2f850b8cc1
Your Name <nizarmu7@gmail.com>
2020-04-15 15:32:15 +0700

    Pembuatan Activity Login(main)

---
+++ app/src/main/java/com/akb/latihan2_10117149/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.akb.latihan2_10117149
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
